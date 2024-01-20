81220b1d54760e05b348b55fba429bc853f849ab
SatanTech <140904777+SatanTech@users.noreply.github.com>
2024-01-20 12:30:45 +0700

    Add files via upload

---
+++ Trojan-GO/addtrgo.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# SL
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+ipsaya=$(curl -sS ipv4.icanhazip.com)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/SatanFusionOfficial/permission/main/ip"
+checking_sc() {
+useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
+if [[ $date_list < $useexp ]]; then
+echo -ne
+else
+echo -e "$COLOR1╭═══════════════════════════════════════════════════╮${NC}"
+echo -e "$COLOR1│ ${NC}${COLBG1}          ${WH}• AUTOSCRIPT PREMIUM •                 ${NC}$COLOR1 │ $NC"
+echo -e "$COLOR1╰═══════════════════════════════════════════════════╯${NC}"
+echo -e "$COLOR1╭═══════════════════════════════════════════════════╮${NC}"
+echo -e "$COLOR1│            ${RED}PERMISSION DENIED !${NC}                  │"
+echo -e "$COLOR1│   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}                             │"
+echo -e "$COLOR1│     \033[0;33mBuy access permissions for scripts${NC}          │"
+echo -e "$COLOR1│             \033[0;33mContact Your Admin ${NC}                 │"
+echo -e "$COLOR1╰═══════════════════════════════════════════════════╯${NC}"
+exit
+fi
+}
+checking_sc
+clear
+uuid=$(cat /etc/trojan-go/uuid.txt)
+source /var/lib/crot/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP
+fi
+trgo="$(cat ~/log-install.txt | grep -w "TrojanGo" | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+		read -rp "Username : " -e user
+		user_EXISTS=$(grep -w $user /etc/trojan-go/akun.conf | wc -l)
+
+		if [[ ${user_EXISTS} == '1' ]]; then
+			echo ""
+			echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
+			exit 1
+		fi
+	done
+read -p "Expired (Days) : " masaaktif
+sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
+systemctl restart trojan-go.service
+link="trojan-go://${user}@${domain}:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#$user"
+clear
+echo -e ""
+echo -e "=======-TROJAN-GO-======="
+echo -e "Remarks    : ${user}"
+echo -e "IP/Host    : ${MYIP}"
+echo -e "Address    : ${domain}"
+echo -e "Port       : ${trgo}"
+echo -e "Key        : ${user}"
+echo -e "Encryption : none"
+echo -e "Path       : /trojango"
+echo -e "Created    : $hariini"
+echo -e "Expired    : $exp"
+echo -e "========================="
+echo -e "Link TrGo  : ${link}"
+echo -e "========================="
+echo -e "Script Mod By SL"
---
+++ Trojan-GO/cektrgo.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# SL
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+ipsaya=$(curl -sS ipv4.icanhazip.com)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/SatanFusionOfficial/permission/main/ip"
+checking_sc() {
+useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
+if [[ $date_list < $useexp ]]; then
+echo -ne
+else
+echo -e "$COLOR1╭═══════════════════════════════════════════════════╮${NC}"
+echo -e "$COLOR1│ ${NC}${COLBG1}          ${WH}• AUTOSCRIPT PREMIUM •                 ${NC}$COLOR1 │ $NC"
+echo -e "$COLOR1╰═══════════════════════════════════════════════════╯${NC}"
+echo -e "$COLOR1╭═══════════════════════════════════════════════════╮${NC}"
+echo -e "$COLOR1│            ${RED}PERMISSION DENIED !${NC}                  │"
+echo -e "$COLOR1│   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}                             │"
+echo -e "$COLOR1│     \033[0;33mBuy access permissions for scripts${NC}          │"
+echo -e "$COLOR1│             \033[0;33mContact Your Admin ${NC}                 │"
+echo -e "$COLOR1╰═══════════════════════════════════════════════════╯${NC}"
+exit
+fi
+}
+checking_sc
+clear
+echo -n > /tmp/other.txt
+data=( `cat /etc/trojan-go/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+echo "------------------------------------";
+echo "-----=[ Trojan-Go User Login ]=-----";
+echo "------------------------------------";
+for akun in "${data[@]}"
+do
+if [[ -z "$akun" ]]; then
+akun="tidakada"
+fi
+echo -n > /tmp/iptrojango.txt
+data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep trojan-go | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
+for ip in "${data2[@]}"
+do
+jum=$(cat /var/log/trojan-go/trojan-go.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
+if [[ "$jum" = "$ip" ]]; then
+echo "$jum" >> /tmp/iptrojango.txt
+else
+echo "$ip" >> /tmp/other.txt
+fi
+jum2=$(cat /tmp/iptrojango.txt)
+sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
+done
+jum=$(cat /tmp/iptrojango.txt)
+if [[ -z "$jum" ]]; then
+echo > /dev/null
+else
+jum2=$(cat /tmp/iptrojango.txt | nl)
+echo "user : $akun";
+echo "$jum2";
+echo "------------------------------------";
+fi
+rm -rf /tmp/iptrojango.txt
+done
+oth=$(cat /tmp/other.txt | sort | uniq | nl)
+echo "other";
+echo "$oth";
+echo "------------------------------------";
+echo ""
+rm -rf /tmp/other.txt
+
